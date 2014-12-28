024ea2ee5df28197f349ac8f6a4990fc86409723
Bogdan Gersak <bogdan@gersak.ro>
2014-12-28 09:24:31 +0100

    Algorithms - Warmup - Is Fibo

---
+++ is_fibo.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPerfectSquare(nr uint64) bool {
+	s := math.Sqrt(float64(nr))
+	fmt.Println("s: ", s, uint64(s), nr)
+	return  uint64(s) * uint64(s) == nr
+}
+
+func isFibo(nr uint64) bool {
+	return isPerfectSquare(5 * nr * nr + 4) || isPerfectSquare(5 * nr * nr - 4)
+}
+
+func isFibo2(nr uint64) bool {
+	var a, b uint64
+	a, b = 1, 2
+
+	for b < nr {
+		a, b = b, a + b
+
+		if b == nr {
+			return true
+		} else if b > nr {
+			return false
+		}
+	}
+
+	return false
+}
+
+// 7778742049
+func main() {
+	var n, nr uint64
+	fmt.Scanf("%v", &n)
+
+	for n > 0 {
+		fmt.Scanf("%v", &nr)
+
+		if nr == 1 || nr == 2 || nr == 3 || isFibo2(nr) {
+			fmt.Println("IsFibo")
+		} else {
+			fmt.Println("IsNotFibo")
+		}
+
+		n--
+	}
+}

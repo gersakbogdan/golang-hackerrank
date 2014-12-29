8c9662247511706c3d72a1cc6df077f918dd249e
Bogdan Gersak <bogdan.gersak@racebets.com>
2014-12-29 09:43:09 +0100

    Algorithms - Arrays and Sorting - Intro to Tutorials Challenges

--- intro_to_tutorial_challenges.go
+++ intro_to_tutorial_challenges.go
@@ -2,6 +2,36 @@ package main
 
 import "fmt"
 
+func binarySearch(arr []uint64, target uint64) int {
+    lo := 0
+    hi := len(arr) - 1
+
+    for lo <= hi {
+        mid := lo + (hi - lo) / 2
+
+        if arr[mid] > target {
+            hi = mid - 1
+        } else if arr[mid] < target {
+            lo = mid + 1
+        } else {
+            return mid
+        }
+    }
+
+    return -1
+}
+
 func main() {
-	fmt.Println("Intro")
+    var n int
+    var v uint64
+
+    fmt.Scan(&v, &n)
+
+    arr := make([]uint64, n)
+
+    for i:=0; i<n; i++ {
+        fmt.Scan(&arr[i])
+    }
+
+    fmt.Println(binarySearch(arr, v))
 }

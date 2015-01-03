32adc630f56a9f0821fa2beb480f33fc9cae9134
Bogdan Gersak <bogdan@gersak.ro>
2015-01-03 18:14:45 +0100

    Algorithms - Arrays and Sorting - Counting sort 2

---
+++ counting_sort_2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func print(arr [100]int) {
+	for i := 0; i < 100; i++ {
+		if (arr[i] > 0) {
+			for j := 0; j < arr[i]; j++ {
+				fmt.Print(i, " ")
+			}
+		}
+	}
+
+	fmt.Println()
+}
+
+func main() {
+	var v, n int
+	var arr [100]int
+
+	io := bufio.NewReader(os.Stdin)
+	fmt.Fscan(io, &n)
+
+	for n > 0 {
+		fmt.Fscan(io, &v)
+		arr[v]++
+		n--
+	}
+
+	print(arr)
+}

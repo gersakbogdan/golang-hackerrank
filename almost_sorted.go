8a205f54b2af91115ffe6e7b4fddeeecc0c2ef91
Bogdan Gersak <bogdan.gersak@racebets.com>
2015-01-09 09:46:51 +0100

    Algorithms - Arrays and Sorting - Almost sorted

--- almost_sorted.go
+++ almost_sorted.go
@@ -1,17 +1,17 @@
 /*
 	Problem Statement
 
-	Given an array with n elements, can you sort this array in ascending order using just one of 
+	Given an array with n elements, can you sort this array in ascending order using just one of
 	the following operations? You can perform only one of the following operations:
 	1. Swap two elements.
 	2. Reverse one sub-segment
 
 	Input Format
-	The first line contains a single integer n, which indicates the size of the array. 
+	The first line contains a single integer n, which indicates the size of the array.
 	The next line contains n integers seperated by spaces.
 
-	n  
-	d1 d2 ... dn  
+	n
+	d1 d2 ... dn
 
 	Constraints
 
@@ -22,7 +22,7 @@
 
 	Output Format
 
-	    If the array is already sorted, output 'yes' in the first line. 
+	    If the array is already sorted, output 'yes' in the first line.
 	    You do not need to output anything else.
 	    If you can sort this array using one single operation (from the two permitted operations):
 
@@ -36,11 +36,11 @@
 	    If you cannot sort the array in either of the above ways, output "no" in the first line.
 
 	Sample Input #1
-	2  
-	4 2  
+	2
+	4 2
 
 	Sample Output #1
-	yes  
+	yes
 	swap 1 2
 
 
@@ -79,9 +79,11 @@ import (
 func insertionSort(arr []int) {
     l := len(arr)
     swaps := 0
+    firstLeftIndex := -1
     leftIndex := -1
     rightIndex := -1
     possible := 1
+    swapMethod := 0
 
     for i := 1; i < l; i++ {
         x := arr[i]
@@ -94,13 +96,20 @@ func insertionSort(arr []int) {
         arr[j] = x
 
         if (j != i) {
-        	if leftIndex >= 0 && (j != leftIndex || i != rightIndex + 1) {
+        	if leftIndex >= 0 && ((j != leftIndex && j != leftIndex+1 && j != firstLeftIndex) || i != rightIndex + 1) {
         		possible = 0
+                break
         	}
+            if j == leftIndex + 1 {
+                swapMethod = 1
+            }
         	leftIndex = j
         	rightIndex = i
-        	fmt.Println(j, i, swaps)
+            if (firstLeftIndex < 0) {
+                firstLeftIndex = j
+            }
         	swaps++
+            fmt.Println("li:", leftIndex, "ri:", rightIndex, "fli:", firstLeftIndex, "j:", j, "i:", i)
         }
     }
 
@@ -110,7 +119,12 @@ func insertionSort(arr []int) {
     	fmt.Println("yes")
     } else if swaps == 1 {
     	fmt.Println("yes")
-    	fmt.Println("swap ", leftIndex + 1, rightIndex + 1)
+    	fmt.Println("swap", leftIndex + 1, rightIndex + 1)
+    } else if swaps > 1 && leftIndex != firstLeftIndex {
+        fmt.Println("no")
+    } else if swaps > 1 && swapMethod == 1 && leftIndex == firstLeftIndex {
+        fmt.Println("yes")
+        fmt.Println("swap", leftIndex + 1, rightIndex + 1)
     } else {
     	fmt.Println("yes")
     	fmt.Println("reverse", leftIndex + 1, rightIndex + 1)
@@ -119,7 +133,6 @@ func insertionSort(arr []int) {
 
 func main() {
     var n int
-
     io := bufio.NewReader(os.Stdin)
     fmt.Fscan(io, &n)
 
